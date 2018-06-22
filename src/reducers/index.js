406d3a9afbfa226ec86c5a98fbf8e60dfb49eea0
EdsonLI <edsonisele@outlook.com>
2018-06-22 18:38:22 -0300

    Concluido ate expansao de linha para detalhamento dos items

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,7 @@ import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
 import SelectionReducer from './SelectionReducer';
 
-
 export default combineReducers({
-    libraries: LibraryReducer,
-    selectedLibraryId: SelectionReducer
+  libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer
 });
-
-// console.log(store.getState());
-// { libraries: [] }
